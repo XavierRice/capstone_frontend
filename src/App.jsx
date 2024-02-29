04379f8ca0398ecffb09e8e61f76e5bad9ad0090
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2024-02-29 10:29:56 -0500

    added folders and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Discover from "./Pages/Discover";
+import NavigationBar from "./Components/NavigationBar";
+import Header from "./Components/Header";
 
 function App() {
- 
-
   return (
-    <>
-     <h1>Capstone Group 2 Activism App</h1>
-    </>
-  )
+    <div>
+      <Router>
+        <Header />
+        <h1 className=" ">Welcome to impactify</h1>
+        <Routes>
+          <Route path="/" element={<Discover />} />
+        </Routes>
+        <NavigationBar />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
