b437b8f4b89c9715cb0955cba377188414ac9684
Xavier Rice <Xavier.Rice@gmail.com>
2024-03-01 17:18:14 -0600

    data rendering-pics are not

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,50 +1,76 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import Card from "../Components/Card"; // Adjust the path as needed
 
 const Events = () => {
   const [loading, setLoading] = useState(true);
   const [eventsData, setEventsData] = useState([]);
+  const [mobilizeEvents, setMobilizeEvents] = useState([]);
+  const [virtualEvents, setVirtualEvents] = useState([])
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    const fetchMobilizeEvents = async () => {
+        try {
+            const response = await axios.get("https://api.mobilize.us/v1/events", {
+                params: {
+                    location: 'New York'
+                }
+            });
+            const events = response.data.data
+            const virtualEvents = events.filter( (event) => event.is_virtual === true)
+            setMobilizeEvents(events)
+            setVirtualEvents(virtualEvents)
+        } catch (error) {
+            console.error('Error fetching Mobilize Events:', error);
+        } finally {
+          setLoading(false)
+        }
+    };
+    fetchMobilizeEvents();
+}, []);
 
-  function fetchData() {
-    setTimeout(() => {
-      const mockData = [
-        {
-          id: 1,
-          title: "New Discoveries in Space",
-          imageSrc: "https://source.unsplash.com/random/800x600/?space",
-          text: "Scientists have made groundbreaking discoveries in the field of astrophysics, shedding new light on the mysteries of the universe. Recent observations from telescopes around the world have revealed...",
-          updatedAt: "Just now",
-        },
-        {
-          id: 2,
-          title: "Climate Change Summit Recap",
-          imageSrc: "https://source.unsplash.com/random/800x600/?climate",
-          text: "World leaders gathered for the annual Climate Change Summit to address the pressing issues facing our planet. Discussions ranged from renewable energy initiatives to...",
-          updatedAt: "1 hour ago",
-        },
-        {
-          id: 1,
-          title: "New Discoveries in Space",
-          imageSrc: "https://source.unsplash.com/random/800x600/?space",
-          text: "Scientists have made groundbreaking discoveries in the field of astrophysics, shedding new light on the mysteries of the universe. Recent observations from telescopes around the world have revealed...",
-          updatedAt: "Just now",
-        },
-        {
-          id: 2,
-          title: "Climate Change Summit Recap",
-          imageSrc: "https://source.unsplash.com/random/800x600/?climate",
-          text: "World leaders gathered for the annual Climate Change Summit to address the pressing issues facing our planet. Discussions ranged from renewable energy initiatives to...",
-          updatedAt: "1 hour ago",
-        },
-      ];
-      setEventsData(mockData);
-      setLoading(false);
-    }, 3000);
-  }
+
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // function fetchData() {
+  //   setTimeout(() => {
+  //     const mockData = [
+  //       {
+  //         id: 1,
+  //         title: "New Discoveries in Space",
+  //         imageSrc: "https://source.unsplash.com/random/800x600/?space",
+  //         text: "Scientists have made groundbreaking discoveries in the field of astrophysics, shedding new light on the mysteries of the universe. Recent observations from telescopes around the world have revealed...",
+  //         updatedAt: "Just now",
+  //       },
+  //       {
+  //         id: 2,
+  //         title: "Climate Change Summit Recap",
+  //         imageSrc: "https://source.unsplash.com/random/800x600/?climate",
+  //         text: "World leaders gathered for the annual Climate Change Summit to address the pressing issues facing our planet. Discussions ranged from renewable energy initiatives to...",
+  //         updatedAt: "1 hour ago",
+  //       },
+  //       {
+  //         id: 1,
+  //         title: "New Discoveries in Space",
+  //         imageSrc: "https://source.unsplash.com/random/800x600/?space",
+  //         text: "Scientists have made groundbreaking discoveries in the field of astrophysics, shedding new light on the mysteries of the universe. Recent observations from telescopes around the world have revealed...",
+  //         updatedAt: "Just now",
+  //       },
+  //       {
+  //         id: 2,
+  //         title: "Climate Change Summit Recap",
+  //         imageSrc: "https://source.unsplash.com/random/800x600/?climate",
+  //         text: "World leaders gathered for the annual Climate Change Summit to address the pressing issues facing our planet. Discussions ranged from renewable energy initiatives to...",
+  //         updatedAt: "1 hour ago",
+  //       },
+  //     ];
+  //     setEventsData(mockData);
+  //     setLoading(false);
+  //   }, 3000);
+  // }
 
   const handleImageLoad = () => {
     setLoading(false);
@@ -58,13 +84,12 @@ const Events = () => {
         </div>
       ) : (
         <div>
-          {eventsData.map((event) => (
+          {virtualEvents.map((event) => (
             <Card
               key={event.id}
               title={event.title}
-              imageSrc={event.imageSrc}
-              text={event.text}
-              updatedAt={event.updatedAt}
+              imageSrc={event.featured_image_url}
+              text={event.summary}
               onLoad={handleImageLoad}
             />
           ))}
