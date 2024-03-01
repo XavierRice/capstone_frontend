32b992f83f7abe10bd4ae10a62c2dbfac17e9a7b
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2024-03-01 12:39:29 -0500

    removed extra discover

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
       <Router>
         <Header />
         <Search />
-        <Discover />
         <div className="content">
           <Routes>
             <Route path="/discover" element={<Discover />} />
