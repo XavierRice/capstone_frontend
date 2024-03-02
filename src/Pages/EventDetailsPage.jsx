95ef593b632a64d0dc65578e1a1892c025a92f75
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2024-03-02 13:52:32 -0500

    adding and styling event details

--- src/Pages/EventDetailsPage.jsx
+++ src/Pages/EventDetailsPage.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 import { useLocation } from "react-router";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 
 function EventDetailsPage() {
-  const location = useLocation()
+  const location = useLocation();
   const { event } = location.state;
+  console.log(event);
 
-
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleDateString();
+  };
 
   return (
     <div>
-      <h2>{event.title}</h2>
-      <img src={event.sponsor.logo_url} alt="Event" />
-      <p>{event.description}</p>
+      <Container>
+        <Row className="mt-4">
+          <Col>
+            <h1 className="header">{event.title}</h1>
+            <p>{event.summary}</p>
+            <p>Date: {formatDate(event.modified_date)}</p>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col xs={12} md={6}>
+            <Image src={event.sponsor.logo_url} alt="Event" fluid />
+          </Col>
+          <Col xs={12} md={6}>
+            <p>{event.description}</p>
+            <p>Location: Virtual</p>
+          </Col>
+        </Row>
+      </Container>
+      <div className="d-flex justify-content-center">
+        <Button
+          variant="primary"
+          size="lg"
+          className="rounded-pill py-2 m-3 d-flex"
+        >
+          Sign Up Now
+        </Button>
+      </div>
+      {/* <Container>
+        Map Stuff can go here
+      </Container> */}
     </div>
-  )
+  );
 }
 
 export default EventDetailsPage;
--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,82 +1,44 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import Card from "../Components/Card"; // Adjust the path as needed
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const Events = () => {
   const [loading, setLoading] = useState(true);
   const [eventsData, setEventsData] = useState([]);
   const [mobilizeEvents, setMobilizeEvents] = useState([]);
-  const [virtualEvents, setVirtualEvents] = useState([])
-  const navigate = useNavigate()
+  const [virtualEvents, setVirtualEvents] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMobilizeEvents = async () => {
-        try {
-            const response = await axios.get("https://api.mobilize.us/v1/events", {
-                params: {
-                    location: 'New York'
-                }
-            });
-            const events = response.data.data
-            const virtualEvents = events.filter( (event) => event.is_virtual === true)
-            setMobilizeEvents(events)
-            setVirtualEvents(virtualEvents)
-        } catch (error) {
-            console.error('Error fetching Mobilize Events:', error);
-        } finally { //finally: This block executes after the try and catch blocks, regardless of the outcome.
-          setLoading(false)
-        }
+      try {
+        const response = await axios.get("https://api.mobilize.us/v1/events", {
+          params: {
+            location: "New York",
+          },
+        });
+        const events = response.data.data;
+        const virtualEvents = events.filter(
+          (event) => event.is_virtual === true
+        );
+        setMobilizeEvents(events);
+        setVirtualEvents(virtualEvents);
+      } catch (error) {
+        console.error("Error fetching Mobilize Events:", error);
+      } finally {
+        //finally: This block executes after the try and catch blocks, regardless of the outcome.
+        setLoading(false);
+      }
     };
     fetchMobilizeEvents();
-}, []);
+  }, []);
 
-const handleCardClick = (event) => {
-  console.log('We are moving...')
-  navigate('/discover/events-details', {state: {event} } )
-}
+  const handleCardClick = (event) => {
+    navigate("/discover/events-details", { state: { event } });
+  };
 
-console.log(virtualEvents)
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // function fetchData() {
-  //   setTimeout(() => {
-  //     const mockData = [
-  //       {
-  //         id: 1,
-  //         title: "New Discoveries in Space",
-  //         imageSrc: "https://source.unsplash.com/random/800x600/?space",
-  //         text: "Scientists have made groundbreaking discoveries in the field of astrophysics, shedding new light on the mysteries of the universe. Recent observations from telescopes around the world have revealed...",
-  //         updatedAt: "Just now",
-  //       },
-  //       {
-  //         id: 2,
-  //         title: "Climate Change Summit Recap",
-  //         imageSrc: "https://source.unsplash.com/random/800x600/?climate",
-  //         text: "World leaders gathered for the annual Climate Change Summit to address the pressing issues facing our planet. Discussions ranged from renewable energy initiatives to...",
-  //         updatedAt: "1 hour ago",
-  //       },
-  //       {
-  //         id: 1,
-  //         title: "New Discoveries in Space",
-  //         imageSrc: "https://source.unsplash.com/random/800x600/?space",
-  //         text: "Scientists have made groundbreaking discoveries in the field of astrophysics, shedding new light on the mysteries of the universe. Recent observations from telescopes around the world have revealed...",
-  //         updatedAt: "Just now",
-  //       },
-  //       {
-  //         id: 2,
-  //         title: "Climate Change Summit Recap",
-  //         imageSrc: "https://source.unsplash.com/random/800x600/?climate",
-  //         text: "World leaders gathered for the annual Climate Change Summit to address the pressing issues facing our planet. Discussions ranged from renewable energy initiatives to...",
-  //         updatedAt: "1 hour ago",
-  //       },
-  //     ];
-  //     setEventsData(mockData);
-  //     setLoading(false);
-  //   }, 3000);
-  // }
+  console.log(virtualEvents);
 
   const handleImageLoad = () => {
     setLoading(false);
