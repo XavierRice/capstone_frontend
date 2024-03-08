a1f9ae3670267f540f726b9cc78f992c59b4ba76
Xavier Rice <Xavier.Rice@gmail.com>
2024-03-08 15:42:46 -0500

    added react-select npm i

--- src/Components/Forms/EventForm.jsx
+++ src/Components/Forms/EventForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link, useNavigate } from "react-router-dom";
-import Select, { createFilter, components} from "react-select"
+import Select, { createFilter, components } from "react-select"
 import AutoComplete from '../GeoLocation/AutoComplete';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
@@ -14,26 +14,28 @@ const backend = import.meta.env.VITE_BACKEND_URL
 const EventForm = () => {
   let { user_id } = useParams();
   const naviagte = useNavigate();
-  
+
   const [selectedKeywords, setSelectedKeywords] = useState([])
   const [location, setLocation] = useState("")
   const [lat, setLat] = useState(0)
   const [lng, setLng] = useState(0)
   const [error, setError] = useState(false)
+  const [userId, setUserId] = useState(user_id)
   const [user_event, setUser_Event] = useState({
+    user_id: userId,
     event_title: "",
-    event_date: "",
-    event_time: "",
+    event_location: location,
     lat: lat,
     lng: lng,
-    event_location: location,
+    event_keyword: "",
     event_details: "",
     event_photo: "",
     is_virtual: false,
-    is_donation: false,
-    event_keyword: "",
+    event_date: "",
+    event_time: "",
     stripe_id: "",
     rsvp: false,
+    is_donation: false,
   });
 
   const keywordOptions = [
@@ -42,6 +44,14 @@ const EventForm = () => {
     { value: 'politics', label: 'politics' },
 
   ]
+
+  const handleKeywords = (selectedOption) => {
+    setSelectedKeywords(selectedOption)
+  }
+  
+  const CreatableSelectInput = (props) => (
+    <components.Input {...props} />
+  );
 
   const handleTextChange = (event) => {
     setEvent({ ...user_event, [event.target.id]: event.target.value })
@@ -54,16 +64,6 @@ const EventForm = () => {
   const handleRSVP = (event) => {
     setEvent({ ...user_event, rsvp: !user_event.rsvp })
   }
-
-  const handleKeywords = (selectedOption) => {
-    setSelectedKeywords(selectedOption)
-  }
-
-  const customFilter = createFilter({ ignoreAccents: false });
-
-  const CreatableSelectInput = (props) => (
-    <components.Input {...props} />
-  );
 
   const addEvent = (event) => {
     fetch(`${backend}/events/${user_id}`, {
@@ -124,13 +124,12 @@ const EventForm = () => {
         <Form.Group as={Col} controlId="event_keyword">
           <Form.Label>Keywords</Form.Label>
           <Select
-          isMulti
-          onChange={handleKeywords}
-          options={keywordOptions}
-          className='basic-mulit-select'
-          classNamePrefix="select"
-          filterOption={customFilter}
-          components={{Input: CreatableSelectInput}}
+            isMulti
+            onChange={handleKeywords}
+            options={keywordOptions}
+            className='basic-mulit-select'
+            classNamePrefix="select"
+            components={{ Input: CreatableSelectInput }}
           />
         </Form.Group>
 
--- src/Pages/Discover.jsx
+++ src/Pages/Discover.jsx
@@ -4,7 +4,7 @@ import CategoriesSection from "../Components/CategoriesSection";
 import ResultsSection from "../Components/ResultsSection";
 
 import SearchBar from "../Components/SearchBar";
-import EventForm from "../Components/Forms/EventForm";
+
 
 function Discover() {
   const [selectedCategory, setSelectedCategory] = useState(null);
