4969f0c78e3a68fe0869e63c7723b49a0a65d1bb
marciafoster <mfoster9tre@gmail.com>
2024-03-19 05:45:41 -0400

    updated paths for input field

--- src/Components/userManagement/LoginForm.jsx
+++ src/Components/userManagement/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import InputField from "./InputField";
+import InputField from "./inputField";
 import ErrorMessage from "./ErrorMessage";
 
 const LoginForm = ({ onSubmit }) => {
--- src/Components/userManagement/SignUpForm.jsx
+++ src/Components/userManagement/SignUpForm.jsx
@@ -1,6 +1,7 @@
+
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import InputField from "./InputField";
+import InputField from "./inputField";
 import ErrorMessage from "./ErrorMessage";
 
 const SignUpForm = ({ onSubmit }) => {
