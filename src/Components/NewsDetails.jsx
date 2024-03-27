5d2d62d3ebd6a61ceeb7ad23b14dfb4a4bb2cbc3
nicole <nicolerodriguezcab@gmail.com>
2024-03-27 17:17:04 -0400

    events layout comeplete - news details updated

--- src/Components/NewsDetails.jsx
+++ src/Components/NewsDetails.jsx
@@ -3,25 +3,34 @@ import { useLocation } from "react-router-dom";
 import RelatedEvents from "./RelatedEvents";
 
 const NewsDetails = () => {
-  const location = useLocation();
-  // const location = useLocation();
-  const { news } = location.state;
+	const location = useLocation();
+	// const location = useLocation();
+	const { news } = location.state;
 
-  const paragraphs = news.news_content.split(/\n\n/);
+	const paragraphs = news.news_content.split(/\n\n/);
 
-  return (
-    <div className="container mt-4">
-      <h2 className="m-3 text-light display-6">{news.news_title}</h2>
-      <img src={news.news_image} className="img-fluid my-3" alt="News" />
+	return (
+		<div className="mx-4 ">
+			<div style={{ margin: "5%" }}>
+				<h2 className="m-3  display-6 d-flex justify-content-center">
+					{news.news_title}
+				</h2>
+				<img
+					src={news.news_image}
+					style={{ borderRadius: "15px" }}
+					className="container"
+					alt="News"
+				/>
 
-      {paragraphs.map((paragraph, index) => (
-        <p key={index} className="text-light m-3">
-          {paragraph}
-        </p>
-      ))}
-      <RelatedEvents />
-    </div>
-  );
+				{paragraphs.map((paragraph, index) => (
+					<p key={index} className=" m-4">
+						{paragraph}
+					</p>
+				))}
+				<RelatedEvents />
+			</div>
+		</div>
+	);
 };
 
 export default NewsDetails;
