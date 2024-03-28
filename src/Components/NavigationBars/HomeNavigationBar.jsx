6d2c5c148a9419a7f83257dcb45b92f5222307ca
nicole <nicolerodriguezcab@gmail.com>
2024-03-28 12:41:20 -0400

    fixed merge conflicts

--- src/Components/NavigationBars/HomeNavigationBar.jsx
+++ src/Components/NavigationBars/HomeNavigationBar.jsx
@@ -1,83 +1,68 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
+	Navbar,
+	Nav,
+	NavDropdown,
+	Form,
+	FormControl,
+	Button,
 } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
 import useScrollPosition from "../../Hooks/ScrollPositionProvider";
-import SearchBar from "../SearchBar";
+import { Link } from "react-router-dom";
 
-
+// eslint-disable-next-line react/prop-types
 function MainNavigationBar({ scrolling = false }) {
+	return (
+		<Navbar
+			bg="light"
+			expand="lg"
+			sticky="top"
+			className={` navbar-shadow navbar-wrapper ${scrolling ? "scrolling" : "not-scrolling"}`}
+			style={{ width: scrolling ? "100%" : "60rem" }}
+		>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-  return (
-    <Navbar
-      bg="light"
-      expand="lg"
-      sticky="top"
-      className={` navbar-shadow ${scrolling ? "scrolling" : "not-scrolling"}`}
-      style={{ width: scrolling ? "100%" : "60rem" }}
-    >
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <FaSearch className="mx-3" style={{ marginRight: "5px" }} />
+			<Link to="#">
+				<FaSearch className="mx-3" style={{ marginRight: "5px" }} />
+			</Link>
+			<Link>
+				<Form
+					to="#"
+					// inline="true"
+				>
+					<FormControl
+						type="text"
+						placeholder="Search"
+						className="mr-sm-2 "
+						style={{ width: "110px" }}
+					/>
+				</Form>
+			</Link>
 
-      {/* <Form inline="true">
-        <FormControl
-          type="text"
-          placeholder="Search"
-          className="mr-sm-2 "
-          style={{ width: "110px" }}
-        /> */}
-      <SearchBar />
-      {/* </Form> */}
-
-// 	return (
-// 		<Navbar
-// 			bg="light"
-// 			expand="lg"
-// 			sticky="top"
-// 			className={` navbar-shadow navbar-wrapper ${scrolling ? "scrolling" : "not-scrolling"}`}
-// 			style={{ width: scrolling ? "100%" : "60rem" }}
-// 		>
-// 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-// 			<FaSearch className="mx-3" style={{ marginRight: "5px" }} />
-
-// 			<Form
-// 			>
-// 				<FormControl
-// 					type="text"
-// 					placeholder="Search"
-// 					className="mr-sm-2 "
-// 					style={{ width: "110px" }}
-// 				/>
-// 			</Form>
-
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown
-            title="For Individuals"
-            id="basic-nav-dropdown"
-            className="mx-3"
-          >
-            <NavDropdown.Item href="#news">About us</NavDropdown.Item>
-            <NavDropdown.Item href="/discover/news">News</NavDropdown.Item>
-            <NavDropdown.Item href="/discover/events">
-              All Events
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#donations">Donations</NavDropdown.Item>
-            <NavDropdown.Item href="#votingtoolkit">
-              Voting Toolkit
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#aboutus">About Us</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Navbar.Brand className="mx-auto display-2" href="/">
-          Impactify
-        </Navbar.Brand>
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<NavDropdown
+						title="For Individuals"
+						id="basic-nav-dropdown"
+						className="mx-3"
+					>
+						<NavDropdown.Item href="#news">About us</NavDropdown.Item>
+						<NavDropdown.Item href="/discover/news">News</NavDropdown.Item>
+						<NavDropdown.Item href="/discover/events">
+							All Events
+						</NavDropdown.Item>
+						<NavDropdown.Item href="#donations">Donations</NavDropdown.Item>
+						<NavDropdown.Item href="#votingtoolkit">
+							Voting Toolkit
+						</NavDropdown.Item>
+						<NavDropdown.Item href="#aboutus">About Us</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+				<Navbar.Brand className="mx-auto display-2" href="/">
+					Impactify
+				</Navbar.Brand>
 
 				<Nav className="ml-auto">
 					<Nav.Link href="#howitworks" className="mx-3">
@@ -86,7 +71,11 @@ function MainNavigationBar({ scrolling = false }) {
 					<Nav.Link href="/discover/users/signup" className="mx-3">
 						Sign In
 					</Nav.Link>
-					<Button variant="" href="/discover/create-event" className="mx-3 btn rounded-pill start-event-btn">
+					<Button
+						variant=""
+						href="/discover/create-event"
+						className="mx-3 btn rounded-pill start-event-btn"
+					>
 						Start Event
 					</Button>
 				</Nav>
