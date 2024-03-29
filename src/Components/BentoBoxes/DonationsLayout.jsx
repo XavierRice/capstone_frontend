89f10a21cf9494d0c384372de871aa5742c79e35
nicole <nicolerodriguezcab@gmail.com>
2024-03-29 15:59:29 -0400

    donations layout is fixed and responsive

--- src/Components/BentoBoxes/DonationsLayout.jsx
+++ src/Components/BentoBoxes/DonationsLayout.jsx
@@ -9,143 +9,96 @@ import { FaArrowRight } from "react-icons/fa";
 
 function DonationsLayout() {
 	return (
-		// <Container fluid style={{ width: "85%", height: "50%", marginTop: "40px" }}>
-		// 	<div className="fw-semibold fs-5 m-3">Featured donations</div>
-		// 	<Row xs={1} md={4} lg={5} className="g-4" style={{ width: "100vw" }}>
-		// 		<Col>
-		// 			<div className="item ">
-		// 				<div className="top-box-grid">
-		// 					<div className="left-grid">
-		// 						<img
-		// 							src={UkraineImg}
-		// 							alt="Ukraine Image"
-		// 							className="top-image"
-		// 						/>
-		// 					</div>
-		// 					<div className="right-grid my-4">
-		// 						<div className="title fs-3 fw-bold">
-		// 							Crisis in Ukraine: How to help
-		// 						</div>
-		// 						<div className="body my-2 fs-4">
-		// 							You can donate to fundraisers on this page to help fleeing
-		// 							families.
-		// 						</div>
-		// 						<div className="my-3">
-		// 							<span className="my-4 ">
-		// 								Donate now <FaArrowRight />
-		// 							</span>
-		// 						</div>
-		// 					</div>
-		// 				</div>
-		// 			</div>
-		// 		</Col>
-		// 	</Row>
-		// 	<Row>
-		// 		<Col>
-		// 			<div className="item mid-right sm-bx item-small ">
-		// 				<div className="top-half">
-		// 					<img
-		// 						src={ClimateImg}
-		// 						alt="Climate Image"
-		// 						className="climate-image"
-		// 					/>
-		// 				</div>
-		// 				<div className="bottom-half">
-		// 					<div className="fw-bold title fs-4 m-3 d-flex justify-content-center">
-		// 						Support Sustainability
-		// 					</div>
-		// 					<span className="m-4 ">
-		// 						Donate now <FaArrowRight />
-		// 					</span>
-		// 				</div>
-		// 			</div>
-		// 		</Col>
-		// 		<Col>
-		// 			<div className="item mid-left item-small ">
-		// 				<div className="top d-flex justify-content-center ">
-		// 					<img src={FactsImg} alt="factsImg" className="climate-image " />
-		// 				</div>
-		// 				<div className="bottom d-flex justify-content-center m-2">
-		// 					<div className="fs-4 fw-bold title">Fundraising FAQs:</div>
-		// 				</div>
-		// 				<div className="m-2">Why should we care and participate?</div>
-		// 				<span className="m-3">
-		// 					Learn more <FaArrowRight />
-		// 				</span>
-		// 			</div>
-		// 		</Col>
-		// 		<Col>
-		// 			<div className="item right item-small ">
-		// 				<img src={DonateCode} alt="Donate Card" className="donate-card" />
-		// 			</div>
-		// 		</Col>
-		// 	</Row>
-		// </Container>
-		<div
-			className=""
-			style={{ width: "85%", height: "50%", marginTop: "40px" }}
+		<Container
+			fluid
+			style={{ width: "85%", height: "100%", marginTop: "40px" }}
 		>
-			<div className="fw-semibold fs-5 m-3">Featured donations</div>
-			<div className="container">
-				<div className="item top-box">
-					<div className="top-box-grid">
-						{" "}
-						<div className="left-grid">
-							<img src={UkraineImg} alt="Ukraine Image" className="top-image" />
-						</div>
-						<div className="right-grid my-4">
-							<div className="title fs-3 fw-bold">
-								Crisis in Ukraine: How to help
+			<div className="fw-semibold fs-5 m-3">Featured</div>
+			<Row xs={1} md={12} lg={8} className="g-4" style={{ width: "100vw" }}>
+				<Col>
+					<div className="item ">
+						<div className="top-box-grid">
+							<div className="left-grid">
+								<img
+									src={UkraineImg}
+									alt="Ukraine Image"
+									className="top-image"
+								/>
 							</div>
-							<div className="body my-2 fs-4">
-								You can donate to fundraisers on this page to help fleeing
-								families.
-							</div>
-							<div className="my-3">
-								<span className="my-4 ">
-									Donate now <FaArrowRight />
-								</span>
+							<div className="right-grid my-4">
+								<div className="title fs-3 fw-bold">
+									Crisis in Ukraine: How to help
+								</div>
+								<div className="body my-2 fs-4">
+									You can donate to fundraisers on this page to help fleeing
+									families.
+								</div>
+								<div className="my-3">
+									<span className="my-4 ">
+										Donate now <FaArrowRight />
+									</span>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-
-				<div className="item mid-right sm-bx item-small ">
-					<div className="top-half">
-						<img
-							src={ClimateImg}
-							alt="Climate Image"
-							className="climate-image"
-						/>
-					</div>
-					<div className="bottom-half">
-						<div className="fw-bold title fs-4 m-3 d-flex justify-content-center">
-							{" "}
-							Support Sustainability
+				</Col>
+			</Row>
+			<Row>
+				<Col xs={12} md={4} lg={4} className="mb-2">
+					<div className="item mid-right item-small ">
+						<div className="top-half">
+							<img
+								src={ClimateImg}
+								alt="Climate Image"
+								className="climate-image"
+							/>
 						</div>
-						<span className="m-4 ">
-							Donate now <FaArrowRight />
+						<div className="bottom-half">
+							<div className="fw-bold title fs-4 d-flex justify-content-center">
+								Support Sustainability
+							</div>
+							<span className="m-4  ">
+								Donate now <FaArrowRight />
+							</span>
+						</div>
+					</div>
+				</Col>
+				<Col s={12} md={4} lg={4} className="mb-2">
+					<div className="item mid-left  item-small">
+						<div className="top d-flex justify-content-center ">
+							<img src={FactsImg} alt="factsImg" className="climate-image " />
+						</div>
+						<div className="d-flex justify-content-center m-2">
+							<div className="fs-4 fw-bold title">Fundraising FAQs:</div>
+						</div>
+						<div className="m-2">Why should we care and participate?</div>
+						<span className="m-3 ">
+							Learn more <FaArrowRight />
 						</span>
 					</div>
-				</div>
-				<div className="item mid-left item-small ">
-					<div className="top d-flex justify-content-center ">
-						<img src={FactsImg} alt="factsImg" className="climate-image " />
+				</Col>
+				<Col s={12} md={4} lg={4} className="mb-2">
+					{/* Replace this with other info */}
+					<div className="item item-small ">
+						<div className="top-half">
+							<img
+								src={ClimateImg}
+								alt="Climate Image"
+								className="climate-image"
+							/>
+						</div>
+						<div className="bottom-half ">
+							<div className="fw-bold title fs-4 d-flex justify-content-center">
+								Support Sustainability
+							</div>
+							<span className="m-4  ">
+								Donate now <FaArrowRight />
+							</span>
+						</div>
 					</div>
-					<div className="bottom d-flex justify-content-center m-2">
-						<div className="fs-4 fw-bold title">Fundraising FAQs:</div>
-					</div>
-					<div className="m-2">Why should we care and participate?</div>
-					<span className="m-3">
-						Learn more <FaArrowRight />
-					</span>
-				</div>
-
-				<div className="item right item-small ">
-					<img src={DonateCode} alt="Donate Card" className="donate-card" />
-				</div>
-			</div>
-		</div>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
