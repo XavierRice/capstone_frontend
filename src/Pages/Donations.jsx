d090a1b537a866495f8803c79e9f6fcc380c206c
Xavier Rice <Xavier.Rice@gmail.com>
2024-03-29 11:41:43 -0400

    first commit

--- src/Pages/Donations.jsx
+++ src/Pages/Donations.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
-import StripeBuy from "../Components/Stripe/StripeBuy";
+import Donation1 from "../Components/Stripe/Donation1";
 import UkraineBuy from "../Components/Stripe/UkraineBuy";
 import CleanUpBuy from "../Components/Stripe/CleanUpBuy";
 import ClimateBuy from "../Components/Stripe/ClimateBuy";
@@ -32,8 +32,8 @@ function Donations() {
     <Container className="py-5 my-5 donations-container">
     <Row className="g-4 justify-content-center">
       <Col xs={12} >
-        
-        <StripeBuy />
+
+        <Donation1 />
       </Col>
       <Col xs={12}>
         {/* Replace with <CardTwo /> or similar */}
