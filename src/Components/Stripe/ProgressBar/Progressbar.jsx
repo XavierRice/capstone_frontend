4781cd55d41bc4ec6a8ade0f30b4893ce88507ba
Xavier Rice <Xavier.Rice@gmail.com>
2024-04-05 13:21:14 -0400

    progress bar

--- src/Components/Stripe/ProgressBar/Progressbar.jsx
+++ src/Components/Stripe/ProgressBar/Progressbar.jsx
@@ -3,11 +3,11 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 import './Progressbar.css'
 
 const Progressbar = ({ donateAmount }) => {
-    const [donationGoal, setDonationGoal] = useState(0)
+    const [donationGoal, setDonationGoal] = useState(500)
     const [verifiedFunds, setVerifiedFunds] = useState(0)
-    const [currentAmount, setCurrentAmount] = useState(800)
+    const [currentAmount, setCurrentAmount] = useState(50)
     //WEBHOOK INTERGRATION FETCH HERE FOR VerifiedFunds/CURRENT AMOUNT FROM STRIPE
-   
+
 
     useEffect(() => {
         setDonationGoal(donateAmount + 10)
@@ -16,14 +16,16 @@ const Progressbar = ({ donateAmount }) => {
     const percentage = Math.min((currentAmount / donationGoal) * 100, 100); // Ensure percentage doesn't exceed 100%
 
     return (
-        <div>
-              <ProgressBar 
-            animated 
-            now={percentage} 
-            label={`${percentage.toFixed(0)}%`} 
-            variant="success" 
-            style={{ width: '50vh', height: '100%', backgroundColor:'#BC9EC1' }} // Adjust the width as necessary, height for visibility
-        />
+        <div className='progress-container'>
+               <span class="current">Current:{currentAmount} </span>
+               <span class="goal">Goal: {donationGoal}</span>
+            <ProgressBar
+                animated
+                now={percentage}
+                label={`${percentage.toFixed(0)}%`}
+                variant="success"
+                style={{ width: '50vh', height: '100%', backgroundColor: '#BC9EC1' }} // Adjust the width as necessary, height for visibility
+            />
         </div>
     );
 };
