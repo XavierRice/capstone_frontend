c2d0fefa3b03f4e0ad63841e6fabfd7ab195df04
nicole <nicolerodriguezcab@gmail.com>
2024-04-09 12:37:50 -0400

    continue to add style sitewide

--- src/Components/CivicApi/VoterModal.jsx
+++ src/Components/CivicApi/VoterModal.jsx
@@ -1,55 +1,58 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 // import VoterTools from './VoterTools'
-import CivicInfo from './CivicInfo';
+import CivicInfo from "./CivicInfo";
 
 const VoterModal = () => {
-    const [userResponse, setUserResponse] = useState(null);
-    const [show, setShow] = useState(true);
-    const handleClose = () => setShow(false);
+	const [userResponse, setUserResponse] = useState(null);
+	const [show, setShow] = useState(true);
+	const handleClose = () => setShow(false);
 
-    const handleYes = () => {
-        setUserResponse('yes');
-        handleClose();
-    };
-    const handleNo = () => {
-        setUserResponse('no');
-        handleClose();
-    };
+	const handleYes = () => {
+		setUserResponse("yes");
+		handleClose();
+	};
+	const handleNo = () => {
+		setUserResponse("no");
+		handleClose();
+	};
 
-    return (
-        <>
-            <Modal
-                show={show}
-                onHide={handleClose}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                     Are you Registered to Vote!
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h4>A Powerful Impact</h4>
-                    <p>
-                    At Impactify, we champion voting as a key tool for change. Every vote shapes our future, so we wanna know: Are you registered to vote? Whether you need to register or are already registered and seeking to connect with local officials, we've got the resources to help you make an impact.
-                    </p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleYes}>
-                        Yes
-                    </Button>
-                    <Button variant="secondary" onClick={handleNo}>
-                        No
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            {userResponse === 'yes' && <CivicInfo />}
-            {/* {userResponse === 'no' && <VoterTools />} */}
-        </>
-    );
+	return (
+		<>
+			<Modal
+				show={show}
+				onHide={handleClose}
+				size="lg"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+			>
+				<Modal.Header closeButton className="">
+					<Modal.Title id="contained-modal-title-vcenter "></Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<h4 className="d-flex justify-content-center ">
+						Let's get you ready for voting season
+					</h4>
+					<p>
+						Every vote shapes our future, so we wanna know: Are you registered
+						to vote? Whether you need to register or are seeking to learn and
+						connect with local officials, we've got the resources to help you
+						make an impact.
+					</p>
+				</Modal.Body>
+				<Modal.Footer className="d-flex justify-content-center ">
+					<Button variant="secondary mx-2" onClick={handleYes}>
+						I am registered
+					</Button>
+					<Button variant="secondary mx-2 " onClick={handleNo}>
+						Register me
+					</Button>
+				</Modal.Footer>
+			</Modal>
+			{userResponse === "yes" && <CivicInfo />}
+			{/* {userResponse === 'no' && <VoterTools />} */}
+		</>
+	);
 };
 
 export default VoterModal;
