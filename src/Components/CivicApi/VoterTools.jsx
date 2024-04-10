87e60bddefee9aac254f523ead26dc1eb5d542c1
Xavier Rice <Xavier.Rice@gmail.com>
2024-04-10 11:31:14 -0400

    updates to widget

--- src/Components/CivicApi/VoterTools.jsx
+++ src/Components/CivicApi/VoterTools.jsx
@@ -1,30 +1,51 @@
-import { useEffect } from 'react';
-import 'https://votinginfotool.org/js/compiled/app.js'
-import '../CivicApi/VoterWidget/VotingCss.css'
+import { useEffect, useState } from 'react';
 
 const VoterTools = () => {
-  
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
-    const loadVIT = () => {
-      if (typeof vit !== 'undefined'){
-        vit.core.init("_vit", {
-          "logo": {"type":"default"},
-          "official-only": true
-        });
-      } else {
-        setTimeout(loadVIT, 500);
-      }
+    const vitStyles = document.createElement('link');
+    vitStyles.rel = 'stylesheet';
+    vitStyles.type = 'text/css';
+    vitStyles.href = 'https://votinginfotool.org/css/compiled/vit.css';
+    document.head.appendChild(vitStyles);
+
+    const script = document.createElement('script');
+    script.src = 'https://votinginfotool.org/js/compiled/app.js';
+    script.onload = () => {
+      const config = {
+        "logo": {"type":"default"},
+        "official-only": true
+      };
+      
+      const loadVIT = () => {
+        if (typeof vit !== 'undefined' && document.getElementById('_vit')){
+          vit.core.init("_vit", config);
+          setIsLoaded(true); // Set isLoaded to true when the script is loaded and the widget is initialized
+        } else {
+          setTimeout(loadVIT, 500);
+        }
+      };
+      
+      loadVIT();
     };
-    loadVIT();
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.head.removeChild(vitStyles);
+    };
   }, []);
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div id="vit-size-container" className="vit-size-container" style={{width: '100%', height: '100%'}}>
       <div id="_vit"></div>
     </div>
   );
-
-
 };
 
 export default VoterTools;
